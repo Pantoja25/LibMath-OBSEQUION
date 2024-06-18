1b21eebe51c606dca44ce0c875a2b67a11e41619
Pantoja25 <165227131+Pantoja25@users.noreply.github.com>
2024-06-18 20:47:53 -0300

    Add 3v

--- Program.cs
+++ Program.cs
@@ -2,50 +2,55 @@
 	internal class Program {
 		static void Main(string[] args) {
 			LibMath libMath = new LibMath();
+			bool continuar = true;
 
-			Console.WriteLine("MENU");
-			Console.WriteLine("1 - Área do Triagolo");
-			Console.WriteLine("2 - Área do Quadrado");
-			Console.WriteLine("3 - Área do Retângulo");
-			Console.WriteLine("4 - Área do Losangolo");
-			Console.WriteLine("5 - Área do Trapézio");
-			Console.WriteLine("6 - Área do Círculo");
-			Console.WriteLine("0 - Sair\n");
+			while (continuar) {
+				Console.Clear();
 
-			Console.WriteLine("Digite o Número:	");
-			int resp = Convert.ToInt16(Console.ReadLine());
+				Console.WriteLine("MENU");
+				Console.WriteLine("1 - Área do Triângulo");
+				Console.WriteLine("2 - Área do Quadrado");
+				Console.WriteLine("3 - Área do Retângulo");
+				Console.WriteLine("4 - Área do Losango");
+				Console.WriteLine("5 - Área do Trapézio");
+				Console.WriteLine("6 - Área do Círculo");
+				Console.WriteLine("0 - Sair\n");
 
-			switch (resp) {
-				case 1:
-					Console.WriteLine("Digite o valor do lado");
+				Console.WriteLine("Digite o Número: ");
+				int resp = Convert.ToInt16(Console.ReadLine());
+
+				switch (resp) {
+					case 1:
+						Console.WriteLine("Digite o valor do lado");
 
 
-					break;
+						break;
 
-				case 2:
-					Console.WriteLine("Digite o valor do lado");
+					case 2:
+						Console.WriteLine("Digite o valor do lado");
 
-					break;
+						break;
 
-				case 3:
-					Console.WriteLine("Digite o valor do lado");
+					case 3:
+						Console.WriteLine("Digite o valor do lado");
 
-					break;
+						break;
 
-				case 4:
-					Console.WriteLine("Digite o valor do lado");
+					case 4:
+						Console.WriteLine("Digite o valor do lado");
 
-					break;
+						break;
 
-				case 5:
-					Console.WriteLine("Digite o valor do lado");
-					break;
+					case 5:
+						Console.WriteLine("Digite o valor do lado");
+						break;
 
-				case 6:
-					Console.WriteLine("Digite o valor do lado");
+					case 6:
+						Console.WriteLine("Digite o valor do lado");
 
-					break;
-
+						break;
+				
+				}
 			}
 		}
 	}
