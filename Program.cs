048963dbbc92433aca00948b5996b1ff41b6af30
Pantoja25 <165227131+Pantoja25@users.noreply.github.com>
2024-06-18 19:43:50 -0300

    Add menu 2v

--- Program.cs
+++ Program.cs
@@ -1,24 +1,52 @@
 ﻿namespace LibMath_OBSEQUION {
 	internal class Program {
 		static void Main(string[] args) {
+			LibMath libMath = new LibMath();
 
-			Console.WriteLine("1- Área do Triagolo");
-			int resp = Convert.ToInt16(Console.ReadLine());
+			Console.WriteLine("MENU");
 
-			LibMath lib = new LibMath();
+			Console.WriteLine("1 - Área do Triagolo\r\n");
+			
+			Console.WriteLine("2 - Área do Quadrado\r\n");
+
+			Console.WriteLine("3 - Área do Retângulo\r\n");
+
+			Console.WriteLine("4 - Área do Losangolo\r\n");
+
+			Console.WriteLine("5 - Área do Trapézio\r\n");
+
+			Console.WriteLine("6 - Área do Círculo\r\n");
+
+			Console.WriteLine("0 - Sair");
+			
 
 			switch (resp) {
 				case 1:
+					Console.WriteLine("Digite o valor do lado");
+
+
+
+					Console.WriteLine("A área do Triangulo");
 					break;
 
 				case 2:
+					Console.WriteLine("Digite o valor do lado");
 					break;
 
 				case 3:
-
+					Console.WriteLine("Digite o valor do lado");
 					break;
+
 				case 4:
-			
+					Console.WriteLine("Digite o valor do lado");
+					break;
+
+				case 5:
+					Console.WriteLine("Digite o valor do lado");
+					break;
+
+				case 6:
+					Console.WriteLine("Digite o valor do lado");
 					break;
 
 			}
