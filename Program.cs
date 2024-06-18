02caf3804601a6d60eb6c73346ae2e206293f380
Pantoja25 <165227131+Pantoja25@users.noreply.github.com>
2024-06-18 19:58:09 -0300

    Add 3v

--- Program.cs
+++ Program.cs
@@ -4,41 +4,37 @@
 			LibMath libMath = new LibMath();
 
 			Console.WriteLine("MENU");
+			Console.WriteLine("1 - Área do Triagolo");
+			Console.WriteLine("2 - Área do Quadrado");
+			Console.WriteLine("3 - Área do Retângulo");
+			Console.WriteLine("4 - Área do Losangolo");
+			Console.WriteLine("5 - Área do Trapézio");
+			Console.WriteLine("6 - Área do Círculo");
+			Console.WriteLine("0 - Sair\n");
 
-			Console.WriteLine("1 - Área do Triagolo\r\n");
-			
-			Console.WriteLine("2 - Área do Quadrado\r\n");
-
-			Console.WriteLine("3 - Área do Retângulo\r\n");
-
-			Console.WriteLine("4 - Área do Losangolo\r\n");
-
-			Console.WriteLine("5 - Área do Trapézio\r\n");
-
-			Console.WriteLine("6 - Área do Círculo\r\n");
-
-			Console.WriteLine("0 - Sair");
-			
+			Console.WriteLine("Digite o Número:	");
+			int resp = Convert.ToInt16(Console.ReadLine());
 
 			switch (resp) {
 				case 1:
 					Console.WriteLine("Digite o valor do lado");
 
 
-
-					Console.WriteLine("A área do Triangulo");
 					break;
 
 				case 2:
 					Console.WriteLine("Digite o valor do lado");
+
 					break;
 
 				case 3:
 					Console.WriteLine("Digite o valor do lado");
+
 					break;
 
 				case 4:
 					Console.WriteLine("Digite o valor do lado");
+
 					break;
 
 				case 5:
@@ -47,6 +43,7 @@
 
 				case 6:
 					Console.WriteLine("Digite o valor do lado");
+
 					break;
 
 			}
